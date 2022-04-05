19d01d6f5ab72e3c63f7f08b48fa6b29b7be27af
evelynvsx <102490601+evelynvsx@users.noreply.github.com>
2022-04-06 10:06:50 +1200

    Add files via upload

--- Lamp.java
+++ Lamp.java
@@ -40,7 +40,7 @@ public class Lamp
     private double left;            //left of the lamp
     private double top;             //top of the lamp
     private double bottom;          //bottom of the lamp
-
+    
     /** Constructor: passed the initial position.
      * Initialises the fields
      */
@@ -51,66 +51,12 @@ public class Lamp
         this.lampSize = size;
         this.lampHeight = stem;
         this.color = col;
-        
-        //set top, left, and bottom
-        this.setTop();
-        this.setLeft();
-        this.setBottom();
-    }
-    
-    /**
-     * Get Left
-     */
-    public double getLeft() {
-        return this.left;
-    }
-    
-    /**
-     * Get Right
-     */
-    public double getRight() {
-        return this.left + this.lampSize;
-    }
-    
-    /**
-     * Get Top
-     */
-    public double getTop() {
-        return this.top;
-    }
-    
-    /**
-     * Get Bottom
-     */
-    public double getBottom() {
-        return this.bottom;
-    }
-    
-    /**
-     * Set left
-     */
-    public void setLeft() {
-        this.left = this.lampX - this.lampSize/2.0;
-    }
-    
-    /**
-     * Set top
-     */
-    public void setTop() {
-        this.top = this.lampY - this.lampSize/2.0;
-    }
-    
-    /**
-     * Set Bottom
-     */
-    public void setBottom() {
-        this.bottom = this.lampY + this.lampHeight;
     }
     
     /**
      * Change the bulb's colour when it is clicked
      */
-    public void clickBulb() {
+    public void changeColor() {
         //change the colour of the bulb to another bright colour
         this.currentColor = Color.getHSBColor((float)(Math.random()), 1.0f, 1.0f);
         UI.setColor(this.currentColor);
@@ -130,7 +76,7 @@ public class Lamp
         UI.drawLine(lampX, lampY, lampX, bottom);       //draw in stem
         
         //draw lamp
-        UI.setColor(this.color);                        //set lamp color
+        UI.setColor(this.currentColor);                 //set lamp color
         UI.fillOval(left, top, lampSize, lampSize);     //draw lamp
         UI.sleep(500);                                  //wait
     }   
@@ -147,12 +93,16 @@ public class Lamp
      * Reports whether the point (x,y) is on the bulb.
      * (x and y represent the position where the mouse was released):
      */
-    //public boolean onBulb(double x, double y){
+    public boolean onBulb(double x, double y){
         // an easy approximation is to pretend it is the enclosing rectangle.
         // It is nicer to do a little bit of geometry and get it right
         /*# YOUR CODE HERE */
+        boolean onBulb = true; 
         
-    //}   
+        if (onBulb) {
+            
+        }
+    }   
 
     /**
      * Reports whether the point (x,y) is on the stem.
@@ -160,32 +110,17 @@ public class Lamp
      */
     //public boolean onStem(double x, double y){
         /*# YOUR CODE HERE */
- 
+        
     //}   
 
     /**
      * Turns the light off.
      * Does not redraw
      */
-    //public void turnOff(){
-        /*# YOUR CODE HERE */
-        
-    //}   
-
-    /** changeColor method (no parameters):
-     * Turns the light on (if it is off)
-     * Changes its color to a random bright colour (if it is already on).
-     * Does not redraw
-     */
-    public void changeColor(){
-        //Erase the flower
-        this.erase();
-        
-        //set the position of the lamp
-        this.setTop();
-        this.setLeft();
-        
-        //changes color of bulb when its clicked
-        this.clickBulb();
-    }   
+    public void turnOff(String stem, double x, double y){
+        //If the stem is clicked, turn off the lamp 
+        if (stem.equals("clicked")) {
+            UI.setColor(Color.black);
+        }
+    }    
 }
--- ObjectsExercise.java
+++ ObjectsExercise.java
@@ -14,47 +14,88 @@ import java.awt.Color;
  */
 
 public class ObjectsExercise {
+    //set the colour of the lamp
+    private Color color;            //color of the lamp
+    private Color currentColor = Color.black;   //set colour of bulb to black since it is off on the start
+    
+    //the lamps
+    private Lamp myLamp;
+    private Lamp yrLamp;
+    
     // Make an array of lamps
     private final int STARTX = 75;
     private final int YPOS = 100;
     private final int MAXLAMPS = 5;
     
-    private Lamp[] lamps = new Lamp[MAXLAMPS];
+    private Lamp[] lampBed = new Lamp[MAXLAMPS];    //array of lamps
     
-    /**
-     * Select object based on where the user clicks
-     */
-    private void doMouse(String action, double x, double y) {
-        if (action.equals("clicked")) {
-            //check the location of the x and y against location of lamp
-            for (Lamp lamp : lamps) {
-                if ((x >= lamp.getLeft()) && (x <= lamp.getRight()) &&
-                (y >= lamp.getTop()) && (y <= lamp.getBottom())) {
-                    lamp.changeColor();
-                }
-            }
-        }
-    }
-    
-
     /** Makes several Lamp objects */
     public void createLamps() {
+        UI.clearPanes();
+        
+        myLamp = new Lamp(-100, -100, 80, 80/4, currentColor);
+        myLamp.draw();
+        yrLamp = new Lamp(-200, -200, 80, 80/4, currentColor);
+        yrLamp.draw();
+        
+        for (int i = 0; i < MAXLAMPS; i++) {
+            //set every flower to a random colour
+            Color col = UI.setColor(Color.black);
+            lampBed[i] = new Lamp(STARTX*(i+1), YPOS, 10, 50, col); //Create the flower object put in array
+        }
+        
+        //draw each flower
+        for (Lamp lamp : lampBed) {
+            lamp.draw();
+        }
     }   
 
     /** Manages the Lamp objects */
     public void manageLamps(String action, double x, double y) {
-
+        if (action.equals("released")) {
+            if (myLamp == null || yrLamp == null) {
+                UI. printMessage("Press Lamps button first to create some lamps");
+                return;     //the lamps haven't been constructed yet
+            }
+            if (myLamp.onBulb(x,y)) {
+                myLamp.changeColor();
+                myLamp.draw();
+            }
+            else if (myLamp.onStem(x,y)) {
+                myLamp.turnOff();
+                myLamp.draw();
+            }
+            else if (yrLamp.onBulb(x,y)) {
+                yrLamp.changeColor();
+                yrLamp.draw();
+            }
+            else if (yrLamp.onStem(x,y)) {
+                yrLamp.turnOff();
+                yrLamp.draw();
+            }
+        }
     }   
 
-
     public void clear() {
-
+        UI.clearPanes();
+        myLamp = null;
+        yrLamp = null;
     }
 
     // Main
     /** Create a new ObjectExercise object and setup the interface */
     public static void main(String[] args) {
-
+        UI.initialise();
+        UI.addButton("Quit", UI::quit);
+        
+        ObjectsExercise obj = new ObjectsExercise();
+        
+        //Add Buttons
+        UI.addButton("Lamps", obj::createLamps);
+        UI.addButton("Clear", obj::clear);
+        
+        //Setup mouse
+        UI.setMouseListener(obj::manageLamps);
     }
 
 }
